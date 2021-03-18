a00717d8cbfb2167dfb85181071715a1065c9cb6
Aeon <aeon_1024@163.com>
2021-03-18 10:56:21 +0800

    update getAvatarByUserId

--- src/service/file.service.js
+++ src/service/file.service.js
@@ -13,8 +13,7 @@ class FileService {
   async getAvatarByUserId(userId) {
     const statement = `SELECT * FROM avatar WHERE user_id = ?;`;
     const [result] = await connections.execute(statement, [userId]);
-    console.log("getAva");
-    return result[0];
+    return result.pop();
   }
   async createFile(filename, mimetype, size, userId, momentId) {
     const statement = `INSERT INTO file (filename, mimetype, size, user_id, moment_id) VALUES (?, ?, ?, ?, ?);`;
